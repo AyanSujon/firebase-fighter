aca045aa4682423ce1f57e61e86a45e8e7a1820f
Ayan Sujon <ayansujonbd@gmail.com>
2025-10-17 01:08:38 +0600

    added signin with google

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,4 +1,4 @@
-import { signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import React, { useState } from 'react';
 import { FaEye } from 'react-icons/fa';
 import { IoEyeOff } from 'react-icons/io5';
@@ -6,12 +6,30 @@ import { Link } from 'react-router';
 import { toast } from 'react-toastify';
 import { auth } from '../firebase/firebase.config';
 
+
+
+const googleProvider = new GoogleAuthProvider();
+
+
 const Signin = () => {
     const [user, setUser] = useState(null);
      const [show, setShow] = useState(false);
     
     
     
+
+     const handleGoogleSignin = ()=> {
+        signInWithPopup(auth, googleProvider)
+       .then(res => {
+                console.log(res);
+                setUser(res.user);
+                toast.success("Signin with Google Successfull!");
+            })
+            .catch((e)=>{
+                console.log(e);
+               toast.error(e.message);
+            })
+     };
     
         const handleSignin = (e)=> {
             e.preventDefault();
@@ -111,7 +129,7 @@ const handleSignOut = ()=>{
                 </div>
 
                 {/* Google */}
-                <button className="btn bg-white text-black border-[#e5e5e5]">
+                <button onClick={handleGoogleSignin} className="btn bg-white text-black border-[#e5e5e5]">
                 <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
                 Login with Google
                 </button>
